265806b359af1dda61c0adb71c4c2f164767298c
Oleksandr_gura <Oleksandr_gura@epam.com>
2016-08-03 11:04:46 +0300

    Added junit

---
+++ src/test/java/SomeTests.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Oleksandr_Gura on 8/3/2016.
+ */
+public class SomeTests {
+
+    @Test
+    public void firstTest(){
+
+        System.out.println("First test");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void secondTest(){
+
+        System.out.println("Second test");
+
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void thirdTest(){
+
+        System.out.println("Third test");
+
+        Assert.assertTrue(true);
+    }
+}
